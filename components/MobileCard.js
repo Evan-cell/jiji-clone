13769ea92316367021ac44b991588b9b6499726a
evan <malcomchege0582@gmail.com>
2024-01-18 15:05:47 +0300

    mobile card

---
+++ components/MobileCard.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { PlusCircleIcon } from '@heroicons/react/24/outline';
+function MobileCard() {
+  return (
+    <div>
+        <div>
+            <PlusCircleIcon/>
+            <h1>Post ad</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/vehicles.png" alt="" />
+            <h1>Vehicles</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/real-estate.png" alt="" />
+            <h1>Property</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/mobile.png" alt="" />
+            <h1>Mobile Phones & Tablets</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/electronics.png" alt="" />
+            <h1>Electronics</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/home.png" alt="" />
+            <h1>Home, Furniture & Appliances</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/beauty.png" alt="" />
+            <h1>Health & Beauty</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/fashion.png" alt="" />
+            <h1>Fashion</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/hobbies.png" alt="" />
+            <h1>Sports,Arts & Outdoors</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/jobseekers.png" alt="" />
+            <h1>Seeking Work CVs</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/services.png" alt="" />
+            <h1>Services</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/jobs.png" alt="" />
+            <h1>Jobs</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/babies.png" alt="" />
+            <h1>Babies & Kids</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/animals.png" alt="" />
+            <h1>Pets</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/agriculture.png" alt="" />
+            <h1>Agriculture & Food</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/equipment.png" alt="" />
+            <h1>Commercial Equipment & Tools</h1>
+        </div>
+        <div>
+            <img src="https://assets.jijistatic.com/art/attributes/categories/repair.png" alt="" />
+            <h1>Repair & Construction</h1>
+        </div>
+        
+    </div>
+  )
+}
+
+export default MobileCard
